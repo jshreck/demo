ab42f97bc701495c5f4966d5141d04d6d9d31695
Julia Shreckhise <jshreckhise@costar.com>
2021-03-23 10:31:54 -0400

    basic app

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import "./App.css";
+import { useState } from "react";
+
+const App = () => {
+  const [color, setColor] = useState("white");
+
+  const handleChange = (event) => {
+    setColor(event.target.value);
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Select a color to change the box color</p>
+        <div
+          style={{ height: "200px", width: "200px", backgroundColor: color }}
+        ></div>
+        <br />
+        <select value={color} onChange={handleChange}>
+          <option value="white">White</option>
+          <option value="pink">Pink</option>
+          <option value="red">Red</option>
+          <option value="blue">Blue</option>
+        </select>
+      </header>
+    </div>
+  );
+};
+
+export default App;
