e158e04d6db6fabf321ed7e5d3cfc20ef563f6f2
Benjamin Hackley <bhackley@costar.com>
2021-03-24 11:12:31 -0400

    Add 'suprise me' option that generates random color

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import "./App.css";
 import { useState } from "react";
 
+const DEFAULT_COLOR_AND_SELECTED_OPTION = "green";
+
 const App = () => {
-  const [color, setColor] = useState("white");
+  const [color, setColor] = useState(DEFAULT_COLOR_AND_SELECTED_OPTION);
+  const [selectedOption, setSelectedOption] = useState(DEFAULT_COLOR_AND_SELECTED_OPTION)
 
   const handleChange = (event) => {
-    setColor(event.target.value);
+    if (event.target.value === "surpriseMe") {
+      setColor(getRandomColor());
+      setSelectedOption(event.target.value);
+    }
+    else {
+      setColor(event.target.value);
+      setSelectedOption(event.target.value);
+    }
   };
+
+  const getRandomColor = () => {
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>Select a color to change the box color</p>
-        <div
-          style={{ height: "200px", width: "200px", backgroundColor: color }}
-        ></div>
+        <div style={{height: "200px", width: "200px", backgroundColor: color}} />
         <br />
-        <select value={color} onChange={handleChange}>
-          <option value="white">White</option>
+        <select value={selectedOption} onChange={handleChange}>
+          <option value="green">Green</option>
           <option value="pink">Pink</option>
           <option value="red">Red</option>
           <option value="blue">Blue</option>
@@ -24,6 +37,7 @@ const App = () => {
           <option value="orange">Orange</option>
           <option value="turquoise">Turquoise</option>
           <option value="gray">Gray</option>
+          <option value="surpriseMe">Surprise Me</option>
         </select>
       </header>
     </div>
